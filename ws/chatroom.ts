7dbbd343ed972ab9c75954024684a802d86665a1
Shaun <shaun@thenetninja.co.uk>
2020-06-08 10:52:59 +0100

    lesson-4

--- ws/chatroom.ts
+++ ws/chatroom.ts
@@ -1,16 +1,30 @@
-import { WebSocket } from 'https://deno.land/std/ws/mod.ts';
+import { WebSocket, isWebSocketCloseEvent } from 'https://deno.land/std/ws/mod.ts';
 import { v4 } from 'https://deno.land/std/uuid/mod.ts';
 
 let sockets = new Map<string, WebSocket>(); 
 
 const chatConnection = async (ws: WebSocket) => {
-  console.log('new socket connection');
-
   // add new ws connection to map
   const uid = v4.generate();
   sockets.set(uid, ws);
 
-  console.log(sockets);
+  // listen for websocket events
+  for await (const ev of ws) {
+    console.log(ev);
+
+    // delete socket if connection closed
+    if (isWebSocketCloseEvent(ev)) {
+      sockets.delete(uid);
+    }
+    
+    // create ev object if ev is string
+    if (typeof ev === 'string') {
+      let evObj = JSON.parse(ev.toString());
+      console.log(evObj);
+    }
+
+  }
+
 };
 
 export { chatConnection };
